f8fa0e4b687d92f2102a7ca5bd5e7b7bc264e2ab
princenishad123 <pn3147399@gmail.com>
2025-12-05 13:35:05 +0530

    fix ui

--- components/header.tsx
+++ components/header.tsx
@@ -47,7 +47,7 @@ export function Header({ title, subtitle }: HeaderProps) {
               <Bell className="w-5 h-5 text-foreground" />
             </button> */}
             <div className="flex items-center gap-2 px-3 py-2 rounded-lg bg-secondary">
-              <div className="w-8 h-8 rounded-full bg-gradient-to-br from-orange-400 to-orange-600 flex items-center justify-center text-white text-sm font-bold">
+              <div className="w-8 h-8 rounded-full bg-linear-to-br from-orange-400 to-orange-600 flex items-center justify-center text-white text-sm font-bold">
                {userFullData?.avtar ? (
                 <img src={userFullData?.avtar} alt={userFullData?.name} className="w-full h-full rounded-full" />
                ) : (
