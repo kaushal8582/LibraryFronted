32848a63aa75a84e19ca2319a831405205714594
developerSTechGit <developers.access.st@gmail.com>
2025-12-25 09:36:35 +0530

    fs

--- lib/Network/api.ts
+++ lib/Network/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig, AxiosResponse, Method } from "axios";
 import toast from "react-hot-toast";
 
 
-const SERVER_URL = 'http://localhost:5002/api'
+const SERVER_URL = 'https://librarybackend-ke0y.onrender.com/api'
 
 // ✅ Create Axios instance with some defaults
 const axiosInstance = axios.create({
